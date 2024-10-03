6f26473327780d3e46b9e71aa2fd465f33a4daca
Alvin2058 <ayush.kc21@apexcollege.edu.np>
2024-10-03 10:40:52 +0545

    Added pagination in the todolist

--- todo_project/todo_list/views.py
+++ todo_project/todo_list/views.py
@@ -6,6 +6,8 @@ from django.utils.dateparse import parse_date
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.utils import timezone
+from django.core.paginator import Paginator,EmptyPage
 
 
 # creating a new to do list
@@ -15,6 +17,13 @@ class TodoCreate(View):
     def post(self, request):
         data = json.loads(request.body)
         try:
+            due_date=parse_date(data.get('due_date'))
+            if due_date is None:
+                return JsonResponse({'error':'please enter a date'})
+            if due_date < timezone.now().date():
+                # print(timezone.now().date)
+                return JsonResponse({'error':'Please enter a valid year'},status=400)
+
             todo = Todo.objects.create(
                 title=data.get('title'),
                 description=data.get('description'),
@@ -22,6 +31,7 @@ class TodoCreate(View):
                 status=data.get('status', 'pending')
             )
             return JsonResponse({'id': todo.id, 'message': 'Todo created successfully'}, status=201)
+
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
 
@@ -30,9 +40,34 @@ class TodoCreate(View):
 class TodoList(View):
     def get(self, request):
         todos = Todo.objects.all().values()
-        return JsonResponse(list(todos), safe=False, status=200)
+        
+        # Gettng the page number and number of tasks ina single page
+        page_num = request.GET.get('page_num', 1)
+        items_in_page = request.GET.get('items_in_page', 5)  
+        # default number of tasks is 5
 
+        # Converting the strings to integers
+        try:
+            page_num = int(page_num)
+            items_in_page = int(items_in_page)
+        except ValueError:
+            return JsonResponse({'error': 'Invalid page number or items per page'}, status=400)
 
+        paginator = Paginator(todos, items_in_page)
+
+        try:
+            todo_page = paginator.page(page_num)
+        except EmptyPage:
+            return JsonResponse({'error': 'No more items'}, status=404)
+
+        # response the user will see 
+        response_data = {
+            'todos': list(todo_page),
+            'total_pages': paginator.num_pages,
+            'current_page': todo_page.number,
+        }
+
+        return JsonResponse(response_data, safe=False, status=200)
 # viewing a specific list of tasks
 class TodoDetail(View):
     def get(self, request, todo_id):
@@ -53,9 +88,13 @@ class TodoDetail(View):
 class TodoUpdate(View):
     def put(self, request, todo_id):
         try:
-            todo = Todo.objects.get(id=todo_id)
             data = json.loads(request.body)
-            
+            todo = Todo.objects.get(id=todo_id)
+
+            due_date=parse_date(data.get('due_date'))
+            if due_date < timezone.now().date():
+                return JsonResponse({'error':'Please enter a valid year'},status=400)
+ 
             todo.title = data.get('title', todo.title)
             todo.description = data.get('description', todo.description)
             todo.due_date = parse_date(data.get('due_date', str(todo.due_date)))
